7149974a0b8e3a4997762b591619cb5a6f1b1b1c
masanocki <sanockimaciej00@gmail.com>
2025-04-14 11:26:41 +0200

    configure sport type

--- model_runner.py
+++ model_runner.py
@@ -13,15 +13,27 @@ if torch.cuda.is_available():
     torch.backends.cudnn.allow_tf32 = True
 
 
-def start_test(model_name, media_path, media_type, device_type, model_type, gui):
+def start_test(
+    model_name, media_path, media_type, device_type, model_type, sport_type, gui
+):
     t1 = threading.Thread(
         target=run_detection,
-        args=(model_name, media_path, media_type, device_type, model_type, gui),
+        args=(
+            model_name,
+            media_path,
+            media_type,
+            device_type,
+            model_type,
+            sport_type,
+            gui,
+        ),
     )
     t1.start()
 
 
-def run_detection(model_name, media_path, media_type, device_type, model_type, gui):
+def run_detection(
+    model_name, media_path, media_type, device_type, model_type, sport_type, gui
+):
     model_name = model_name.lower()
     media_path = Path(media_path)
 
@@ -47,13 +59,13 @@ def run_detection(model_name, media_path, media_type, device_type, model_type, g
             run_yolo_coco_images(model_name, media_path, device, gui)
 
         case ("yolov11", "videos", "custom"):
-            run_yolo_custom_videos(model_name, media_path, device, gui)
+            run_yolo_custom_videos(model_name, media_path, device, sport_type, gui)
         case ("yolov11", "images", "custom"):
-            run_yolo_custom_images(model_name, media_path, device, gui)
+            run_yolo_custom_images(model_name, media_path, device, sport_type, gui)
         case ("yolov12", "videos", "custom"):
-            run_yolo_custom_videos(model_name, media_path, device, gui)
+            run_yolo_custom_videos(model_name, media_path, device, sport_type, gui)
         case ("yolov12", "images", "custom"):
-            run_yolo_custom_images(model_name, media_path, device, gui)
+            run_yolo_custom_images(model_name, media_path, device, sport_type, gui)
 
         case ("ssd", "videos", "coco"):
             run_ssd_coco_videos(media_path, device, gui)
@@ -61,9 +73,9 @@ def run_detection(model_name, media_path, media_type, device_type, model_type, g
             run_ssd_coco_images(media_path, device, gui)
 
         case ("ssd", "videos", "custom"):
-            run_ssd_custom_videos(media_path, device, gui)
+            run_ssd_custom_videos(media_path, device, sport_type, gui)
         case ("ssd", "images", "custom"):
-            run_ssd_custom_images(media_path, device, gui)
+            run_ssd_custom_images(media_path, device, sport_type, gui)
 
         case ("efficientdet", "videos", "coco"):
             run_efficientdet_coco_videos(media_path, device, gui)
@@ -71,9 +83,9 @@ def run_detection(model_name, media_path, media_type, device_type, model_type, g
             run_efficientdet_coco_images(media_path, device, gui)
 
         case ("efficientdet", "videos", "custom"):
-            run_efficientdet_custom_videos(media_path, device, gui)
+            run_efficientdet_custom_videos(media_path, device, sport_type, gui)
         case ("efficientdet", "images", "custom"):
-            run_efficientdet_custom_images(media_path, device, gui)
+            run_efficientdet_custom_images(media_path, device, sport_type, gui)
 
         case ("rt-detrv2", "videos", "coco"):
             run_rtdetrv2_coco_videos(media_path, device, gui)
@@ -81,9 +93,9 @@ def run_detection(model_name, media_path, media_type, device_type, model_type, g
             run_rtdetrv2_coco_images(media_path, device, gui)
 
         case ("rt-detrv2", "videos", "custom"):
-            run_rtdetrv2_custom_videos(media_path, device, gui)
+            run_rtdetrv2_custom_videos(media_path, device, sport_type, gui)
         case ("rt-detrv2", "images", "custom"):
-            run_rtdetrv2_custom_images(media_path, device, gui)
+            run_rtdetrv2_custom_images(media_path, device, sport_type, gui)
 
         case _:
             return
